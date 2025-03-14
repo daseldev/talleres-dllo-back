0d8d591bb2328f6cdbf21dbd01396e4e70514e57
Daniel Felipe Sierra Forero <danifscuentas@gmail.com>
2025-03-14 13:40:50 -0500

    Update taller3.js

--- taller3.js
+++ taller3.js
@@ -35,9 +35,9 @@ function twoSum(nums, target) {
 }
 
 // Pruebas
-console.log(allTwoSum([2, 7, 11, 15], 9)); 
-console.log(allTwoSum([3, 4, 2, 4], 6));    
-console.log(allTwoSum([1, 2, 3, 4, 5], 6)); 
+console.log(twoSum([2, 7, 11, 15], 9)); 
+console.log(twoSum([3, 4, 2, 4], 6));    
+console.log(twoSum([1, 2, 3, 4, 5], 6)); 
 
 
 function conversionRomana(romano) {
